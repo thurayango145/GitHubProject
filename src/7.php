850621a079202e9b2d02d384e57423d7dff559b2
thurayango145 <thurayango145@gmail.com>
2025-03-11 22:08:44 +0000

    Add 7.php

---
+++ src/7.php
@@ -0,0 +1,17 @@
+<?php
+
+function getRandomNumber($min, $max) {
+    return mt_rand($min, $max);
+}
+
+function getRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+?>
