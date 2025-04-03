1ceca5880d5579a6f6f09644e262dbdbee2a7f83
thurayango145 <thurayango145@gmail.com>
2025-04-03 13:12:16 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,23 @@
+<?php
+// This is an example PHP script to demonstrate basic coding concepts.
+// Feel free to modify and extend it according to your needs!
+
+// Define variables
+$name = "John Doe";
+$age = 25;
+$message = "Hello, World!";
+echo $name . ", " . $message . "!";
+
+// Example PHP code demonstrating basic functions
+
+function add($a, $b) {
+    return $a + $b;
+}
+
+$sum = add(10, 5);
+echo "The sum of $a and $b is $sum.";
+
+// Print a message to the console
+echo "Message: " . $_GET['message'] ?? "";
+
+?>
