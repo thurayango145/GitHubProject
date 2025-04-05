87794465cd12e8ccd32b08f34a2f3b123b0067dd
thurayango145 <thurayango145@gmail.com>
2025-04-05 06:04:32 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,13 @@
+<?php
+// Set up your database connection here
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
