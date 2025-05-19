b0699086fed63c19fc772b57e080137bdad573da
thurayango145 <thurayango145@gmail.com>
2025-05-19 12:59:38 +0000

    Add 42.php

---
+++ src/42.php
@@ -0,0 +1,18 @@
+<?php
+// Initialize PHP session if you haven't already
+session_start();
+
+// Your code here
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "school_project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
